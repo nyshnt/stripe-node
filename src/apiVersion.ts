cfcc55313f576a96ea7af8ce539e759e4aa05fc9
Nishant Kumar <nishantmbp@Nishants-MacBook-Pro.local>
2023-03-28 12:09:45 +0530

    required changes

--- src/apiVersion.ts
+++
@@ -1,3 +0,0 @@
-// File generated from our OpenAPI spec
-
-export const ApiVersion = '2022-11-15';
--- test/crypto/NodeCryptoProvider.spec.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const {NodeCryptoProvider} = require('../../cjs/crypto/NodeCryptoProvider.js');
-
-const {createCryptoProviderTestSuite} = require('./helpers.js');
-
-describe('NodeCryptoProvider', () => {
-  createCryptoProviderTestSuite(new NodeCryptoProvider());
-});
